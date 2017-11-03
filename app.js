64c0c61c5786516ef8e90c42798ffccb3d3eacc2
juankboards <juankboards@gmail.com>
2017-11-03 15:57:49 -0400

    https redirect

--- app.js
+++ app.js
@@ -21,12 +21,13 @@ app.use(function(req, res, next) {
 });
 
 // app.configure('production', () => {
-//   app.use((req, res, next) => {
-//     if (req.header 'x-forwarded-proto' !== 'https')
-//       res.redirect(`https://${req.header('host')}${req.url}`)
-//     else
-//       next()
-//   })
+  app.use((req, res, next) => {
+    console.log(req.header['x-forwarded-proto'])
+    // if (req.header 'x-forwarded-proto' !== 'https')
+    //   res.redirect(`https://${req.header('host')}${req.url}`)
+    // else
+    //   next()
+  })
 // })
 
 require('./app/routes')(app);    
